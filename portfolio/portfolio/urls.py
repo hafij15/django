5124093994e9646e85c75dacd7ffbfe5a1d391c5
Hafijur Rahman <hafij.sabuj@gmail.com>
2021-10-31 17:49:21 +0600

    Template mastering is complete.
    {% url 'home' %}
    {% static 'img/profile/profile.jpg' %}
    
    IN VIEWS.PY
    from django.shortcuts import render
    from django.http import HttpResponse
    def contact(request):
        #return HttpResponse("This is our contact page")
        return render (request, '../templates/pages/contact.html')
    
    IN URLS.PY
    from django.contrib import admin
    from django.urls import path,include #here define the include for link child app.
    from . import views
    urlpatterns = [
        path('blog-details/', views.blogDetails, name='blog-details'),
        path('employee/', include('employee.urls')), # using this include connect with the main app portfolio.
    ]

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -7,5 +7,13 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('aboutus/', views.about, name='about'),
     path('contactus/', views.contact, name='contact'),
+    path('team/', views.team, name='team'),
+    path('testimonial/', views.testimonial, name='testimonial'),
+    path('services/', views.services, name='services'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('portfolio-details/', views.portfolioDetails, name='portfolio-details'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('blog/', views.blog, name='blog'),
+    path('blog-details/', views.blogDetails, name='blog-details'),
     path('employee/', include('employee.urls')), # using this include connect with the main app portfolio.
 ]
--- portfolio/portfolio/views.py
+++ portfolio/portfolio/views.py
@@ -6,7 +6,31 @@ def home(request):
     return render(request, 'index.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, '../templates/pages/about.html')
 
 def contact(request):
-    return render (request, 'contact.html')
+    return render (request, '../templates/pages/contact.html')
+
+def team(request): 
+    return render (request, '../templates/pages/team.html')
+
+def testimonial(request): 
+    return render (request, '../templates/pages/testimonial.html')
+
+def services(request): 
+    return render(request, '../templates/pages/services.html')
+
+def portfolio(request): 
+    return render(request, '../templates/pages/portfolio.html')
+
+def portfolioDetails(request): 
+    return render(request, '../templates/pages/portfolio-details.html')
+
+def pricing(request): 
+    return render(request, '../templates/pages/pricing.html')
+
+def blog(request): 
+    return render(request, '../templates/pages/blog.html')
+
+def blogDetails(request): 
+    return render(request, '../templates/pages/blog-details.html')
